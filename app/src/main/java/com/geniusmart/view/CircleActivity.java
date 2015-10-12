3a50e1a9f0c62706e709140e5d2d792bfd8867cf
GeniusWXCY <112151zz>
2015-10-13 00:06:34 +0800

    commit

--- app/src/main/java/com/geniusmart/view/CircleActivity.java
+++ app/src/main/java/com/geniusmart/view/CircleActivity.java
@@ -3,10 +3,13 @@ package com.geniusmart.view;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.geniusmart.R;
+
 public class CircleActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle);
     }
 }
--- app/src/main/java/com/geniusmart/view/CircleView.java
+++ app/src/main/java/com/geniusmart/view/CircleView.java
@@ -37,6 +37,23 @@ public class CircleView extends View{
         int width = getWidth();
         int height = getHeight();
         int radius = Math.min(width,height)/2;
-        canvas.drawCircle(width,height,radius,mPaint);
+        canvas.drawCircle(width/2,height/2,radius,mPaint);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSpecMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        if(widthSpecMode == MeasureSpec.AT_MOST && heightSpecMode == MeasureSpec.AT_MOST){
+            setMeasuredDimension(200, 200);
+        }else if (widthSpecMode == MeasureSpec.AT_MOST){
+            setMeasuredDimension(200, heightSpecSize);
+        }else if(heightMeasureSpec == MeasureSpec.AT_MOST){
+            setMeasuredDimension(widthSpecSize,200);
+        }
     }
 }
