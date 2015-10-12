204fe5bb10a83c2fece5bf9e5f5e993986595a03
GeniusWXCY <112151zz>
2015-10-12 23:43:42 +0800

    commit

---
+++ app/src/main/java/com/geniusmart/view/CircleActivity.java
@@ -0,0 +1,12 @@
+package com.geniusmart.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CircleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/geniusmart/view/CircleView.java
@@ -0,0 +1,42 @@
+package com.geniusmart.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class CircleView extends View{
+
+    private int mDefaultColor = Color.RED;
+    private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    public CircleView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CircleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CircleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init(){
+        mPaint.setColor(mDefaultColor);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int width = getWidth();
+        int height = getHeight();
+        int radius = Math.min(width,height)/2;
+        canvas.drawCircle(width,height,radius,mPaint);
+    }
+}
