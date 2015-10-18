5095986cf8b9bca736b3e66ca51de4953489dac2
geniusmart <112151zz>
2015-10-18 23:55:33 +0800

    commit

--- app/src/main/java/com/geniusmart/view/CircleActivity.java
+++ app/src/main/java/com/geniusmart/view/CircleActivity.java
@@ -2,14 +2,32 @@ package com.geniusmart.view;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.geniusmart.R;
 
 public class CircleActivity extends Activity {
 
+    public static final String TAG = "CircleActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circle);
+        final CircleView circleView = (CircleView) findViewById(R.id.circle_view);
+        //TODO 问题：measure过程和生命周期不是同步执行的？那么分别是在哪个线程中执行的？？
+        Log.e(TAG, circleView.getWidth() + "");
+        Log.e(TAG, circleView.getHeight() + "");
+        Log.e(TAG, circleView.getX() + "");
+        Log.e(TAG, circleView.getY() + "");
+        circleView.post(new Runnable() {
+            @Override
+            public void run() {
+                Log.e(TAG, circleView.getWidth() + "");
+                Log.e(TAG, circleView.getHeight() + "");
+                Log.e(TAG,circleView.getX()+"");
+                Log.e(TAG,circleView.getY()+"");
+            }
+        });
+
     }
 }
--- app/src/main/java/com/geniusmart/view/CircleView.java
+++ app/src/main/java/com/geniusmart/view/CircleView.java
@@ -5,12 +5,14 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class CircleView extends View{
 
     private int mDefaultColor = Color.RED;
     private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private static final String TAG = "CircleView";
 
     public CircleView(Context context) {
         super(context);
@@ -36,12 +38,26 @@ public class CircleView extends View{
         super.onDraw(canvas);
         int width = getWidth();
         int height = getHeight();
+
+        int paddingLeft = getPaddingLeft();
+        int paddingRight = getPaddingRight();
+        int paddingTop = getPaddingTop();
+        int paddingBottom = getPaddingBottom();
+        width = width - paddingLeft - paddingRight;
+        height = height - paddingBottom - paddingTop;
         int radius = Math.min(width,height)/2;
-        canvas.drawCircle(width/2,height/2,radius,mPaint);
+        canvas.drawCircle(paddingLeft + width / 2, paddingTop + height / 2, radius, mPaint);
+        Log.e(TAG, width + "");
+        Log.e(TAG, height + "");
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int widthSpecMode = MeasureSpec.getMode(widthMeasureSpec);
         int widthSpecSize = MeasureSpec.getSize(widthMeasureSpec);
